31e5f2b22a9acc06a75608e936bf36480a5d9530
Krishna <kn1g23@soton.ac.uk>
2025-02-25 12:18:49 +0000

    Added all javafx plugins and dependencies

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class App extends Application {
+
+  private static App instance;
+  private static final Logger logger = LogManager.getLogger(App.class);
+  private Stage stage;
+  public static void main(String[] args){
+    logger.info("Launching app");
+    launch();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    instance = this;
+    this.stage = stage;
+
+  }
+
+  public static App getInstance() {
+    return instance;
+  }
+
+}
