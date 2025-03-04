2cfdffc9ed62037dc5f3f09693c8f7d3fd8ab4d3
sfsb1d22 <sfsb1d22@soton.ac.uk>
2025-03-04 00:19:07 +0000

    Added a login page, also a section to upload the csv files

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -22,13 +22,15 @@ public class App extends Application {
   public void start(Stage stage) throws Exception {
     instance = this;
     this.stage = stage;
-    StackPane root = new StackPane();
-    Label welcomeMessage = new Label("Ad Auction");
-    root.getChildren().add(welcomeMessage);
-    Scene scene = new Scene(root, 800, 600);
-    stage.setScene(scene);
-    stage.show();
-    logger.info("App started");
+    Login login = new Login(stage);
+    login.show();
+    logger.info("Login completed");
+  }
+
+  public void showInputFilesPage(){
+    InputFilesPage inputFilesPage = new InputFilesPage(stage);
+    inputFilesPage.show();
+    logger.info("Input files page");
   }
 
   public static App getInstance() {
