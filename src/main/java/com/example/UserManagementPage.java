20cd146a3404bbd937124595ac38cf69a65d00dd
sfsb1d22 <sfsb1d22@soton.ac.uk>
2025-04-05 06:07:13 +0300

    Fixed error

--- src/main/java/com/example/UserManagementPage.java
+++ src/main/java/com/example/UserManagementPage.java
@@ -2,7 +2,6 @@ package com.example;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
